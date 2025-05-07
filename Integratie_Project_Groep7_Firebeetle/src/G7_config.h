cb64c0488533f591d00fdf773a2849d58b68fe1f
EarthSword28 <65962366+EarthSword28@users.noreply.github.com>
2025-05-07 14:19:48 +0200

    config + structure

---
+++ Integratie_Project_Groep7_Firebeetle/src/G7_config.h
@@ -0,0 +1,1 @@
+// zet hier alle constanten
--- Integratie_Project_Groep7_Firebeetle/src/main.cpp
+++ Integratie_Project_Groep7_Firebeetle/src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+void G7_setup() {}
+
+void G7_loop() {}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  G7_setup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  G7_loop();
 }
